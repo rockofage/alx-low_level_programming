07aad8717292eea3ddd7c87a58cd012e136da9b5
rockofage <erockson199@gmail.com>
2023-03-21 19:46:22 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print all seconds of the day 
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+
+	while (hours < 24)
+	{
+		int mins = 0;
+
+		while (mins < 60)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+			mins++;
+		}
+		hours++;
+	}
+}
