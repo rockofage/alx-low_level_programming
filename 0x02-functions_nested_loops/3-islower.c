85c1c62add54f0036b00b8fe750651debd994547
rockofage <erockson199@gmail.com>
2023-03-21 18:15:25 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,18 @@
 /**
  * _islower - checks for lowercase characters
  *
+ * Parameters: interger value of a character
+ *
  * Return: 1 if lowercase and 0 if otherwise
  */
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
