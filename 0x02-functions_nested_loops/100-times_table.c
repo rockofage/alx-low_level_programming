2ea0428a5ce3a9fd854aa4e4120e1316ffb13bad
rockofage <erockson199@gmail.com>
2023-03-21 22:30:40 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table
+ *@n: number of times table
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	if (n >= 0 && n <= 15)
+	{
+		int i = 0;
+
+		while (i <= n)
+		{
+			int j = 0;
+
+			while (j <= n)
+			{
+				int p = i * j;
+				int di = 100;
+
+				if (j > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				while ((p / di) == 0)
+				{
+					_putchar(' ');
+					di /= 10;
+				}
+
+				while (di >= 1)
+				{
+					_putchar((p / di) + '0');
+					p = p % di;
+					di /= 10;
+				}
+				_putchar('\n');
+				j++;
+			}
+			i++;
+		}
+	}
+}
