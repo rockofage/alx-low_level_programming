eb0b1f966c43f20403ff46799e1c050761af7db2
rockofage <erockson199@gmail.com>
2023-03-21 17:54:09 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet_x10 - print alphabets 10x
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j = 'a';
+
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
