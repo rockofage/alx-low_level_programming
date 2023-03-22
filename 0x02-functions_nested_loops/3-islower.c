dce57ca1d47e03feca82f0cc4f36362fa894268f
rockofage <erockson199@gmail.com>
2023-03-21 18:12:05 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ *
+ * Return: 1 if lowercase and 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
