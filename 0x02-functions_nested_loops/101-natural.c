5534538026d8b1ed1af23fb0ea7a5b5c825e8d7b
rockofage <erockson199@gmail.com>
2023-03-21 23:56:54 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,22 +11,32 @@ void sum_natural(void)
 	mul_3 = mul_5 = mul_15 = sum_3 = sum_5 = sum_15 = 0;
 
 	while (mul_3 < 1024)
+	{
 		sum_3 += mul_3;
 		mul_3 += 3;
+	}
 	while (mul_5 < 1024)
+	{
 		sum_5 += mul_5;
 		mul_5 += 5;
+	}
 	while (mul_15 < 1024)
+	{
 		sum_15 += mul_15;
 		mul_15 += 15;
+	}
 
 	int final_answer = sum_3 + sum_5 - sum_15;
 	int final_answer_reverse = 0;
 	
 	while (final_answer / 10 != 0 || final_answer % 10 != 0)
+	{
 		final_answer_reverse = (final_answer_reverse * 10) + (final_answer % 10);
 		final_answer = final_answer / 10;
+	}
 	while (final_answer_reverse % 10 != 0 || final_answer_reverse / 10 != 0)
+	{
 		_putchar((final_answer_reverse % 10) + '0');
 		final_answer_reverse = final_answer_reverse / 10;
+	}
 }
