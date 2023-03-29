1dff8e1d38f1d30b5f90d66d48b04b87b56e163d
rockofage <erockson199@gmail.com>
2023-03-28 20:11:32 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i, counter;
 
-	while (*(src + i) != '\0')
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-	dest[i + 1] = src[i + 1];
+	i = counter = 0;
+	while (src[counter] != '\0')
+		counter++;
+	for (i = 0; i <= counter; i++)
+		dest[i] = src[i];
 	return (dest);
 }
