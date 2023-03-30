1b181043a0a2bb4c7a0b48687c99e7263b76d01b
rockofage <erockson199@gmail.com>
2023-03-30 06:57:43 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,22 +9,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, n1, n2;
+	char c1, c2;
 
-	for (n1 = 0; s1[n1] != '\0'; n1++)
-		continue;
-	for (n2 = 0; s2[n2] != '\0'; n2++)
-		continue;
-	for (i = 0; s1[i] == s2[i]; i++)
-	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-			return (0);
-	}
-	if (n1 > n2)
-		return (15);
-	if (n1 < n2)
-		return (-15);
-	if (s1[i] > s2[i])
-		return (15);
-	return (-15);
+	do {
+		c1 = *s1++;
+		c2 = *s2++;
+		if (c1 == '\0')
+			return (c1 - c2);
+	} while (c1 == c2);
+	return (c1 - c2);
 }
