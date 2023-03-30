b7fc15f15450de895870796cbd4b8d028c9d3815
rockofage <erockson199@gmail.com>
2023-03-30 13:37:58 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encrypts a string
+ * @s: string
+ *
+ * Return: edited @s
+ */
+char *leet(char *s)
+{
+	int i, j;
+gchar *ori, *new;
+
+	ori = "aAeEoOtTlL";
+	new = "4433007711";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; ori[j] != '\0'; j++)
+		{
+			if (s[i] == ori[j])
+			{
+				s[i] = new[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
