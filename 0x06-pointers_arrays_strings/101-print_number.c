b24fef97378c5380cef21de9e84fae80c78a5369
rockofage <erockson199@gmail.com>
2023-03-30 19:20:45 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,33 +8,12 @@
  */
 void print_number(int n)
 {
-	int counter, i, div;
-	unsigned int nc;
-
-	div = 1;
-	counter = 0;
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n = -n;
 	}
-	else if (n == 0)
-		_putchar('0');
-	if (n > 0)
-	{
-		nc = n;
-		while (nc > 0)
-		{
-			counter++;
-			nc = nc / 10;
-		}
-		for (i = 1; i < counter; i++)
-			div = div * 10;
-		while (div > 0)
-		{
-			_putchar(n / div + '0');
-			n = n % div;
-			div = div / 10;
-		}
-	}
+	if (n / 10 > 0)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
