29ece6b517a8ba08e9839a2564348492deb78392
rockofage <erockson199@gmail.com>
2023-04-04 16:30:12 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,14 +7,15 @@
  *
  * Return: pointer to the first occurence of @c otherwise, NULL
  */
+
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return (&s[i]);
+			return (s + i);
 	}
-	return (0);
+	return ('\0');
 }
