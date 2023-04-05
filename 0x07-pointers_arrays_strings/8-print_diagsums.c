441b5c2e19014bc64e40838daa2ea829ad2a1a89
rockofage <erockson199@gmail.com>
2023-04-04 20:03:59 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sums the figures in the diag of a sq. matrix
+ * @a: square matrix
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	unsigned long int sum1, sum2;
+
+	sum1 = a[0];
+	sum2 = a[size -1];
+	for (i = 1; i < size; i++)
+	{
+		sum1 += a[size * i + i];
+		sum2 += a[size * (i + 1) - (i +1)];
+	}
+	printf("%lu, %lu\n", sum1, sum2);
+}
