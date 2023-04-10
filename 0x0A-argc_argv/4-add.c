7f7e4bddda3dd9f0d126f56bca18d77ab95b5e9f
rockofage <erockson199@gmail.com>
2023-04-09 19:06:51 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positve numbers
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: results
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) != 0)
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
